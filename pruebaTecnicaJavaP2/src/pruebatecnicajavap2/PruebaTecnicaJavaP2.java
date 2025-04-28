910b70e0c400078142431332001ad9972a5ab3f3
kba1986cr <haroldsa1986@hotmail.com>
2025-04-28 16:58:53 -0600

    Actualizo

--- pruebaTecnicaJavaP2/src/pruebatecnicajavap2/PruebaTecnicaJavaP2.java
+++ pruebaTecnicaJavaP2/src/pruebatecnicajavap2/PruebaTecnicaJavaP2.java
@@ -5,6 +5,7 @@
 package pruebatecnicajavap2;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import pruebatecnicajavap2.logica.Ticket;
 
@@ -20,9 +21,31 @@ public class PruebaTecnicaJavaP2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        
+        //Estrudtura de Datos
         List listaTickets = new ArrayList <Ticket>();
         
-        Ticket ticket1 = new Ticket(0, 0, 0, 0, fechaCompra, fechaValidez);
+        //Tickets cn constructor
+        Ticket ticket1 = new Ticket(12, 2, 3, 1500, new Date(), new Date());
+        Ticket ticket2 = new Ticket(13, 2, 4, 1500, new Date(), new Date());
+        
+        //Ticket cn constructor vacio y encapsulamiento
+        Ticket ticket3 = new Ticket();
+        ticket3.setNumero(20);
+        ticket3.setFila(5   );
+        ticket3.setAsiento(10);
+        ticket3.setPrecio(2300);
+        ticket3.setFechaCompra(new Date());
+        ticket3.setFechaValidez(new Date());
+    
+        //Carga de tickets a la lista
+        listaTickets.add(ticket1);
+        listaTickets.add(ticket2);
+        listaTickets.add(ticket3);
+        
+        
+        
+        
         
     }
     
